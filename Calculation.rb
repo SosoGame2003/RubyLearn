833fdc2c8d43d51c257805a9b15e59391005793e
Scarlet20030225 <21010022hs@edu.fca.ac.jp>
2023-10-20 18:03:47 +0900

    お初のRuby

---
+++ Calculation.rb
@@ -0,0 +1,15 @@
+#足し算
+addition = 100 + 100
+puts addition
+
+#引き算
+subtraction = 100 - 100
+puts subtraction
+
+#掛け算
+multiplication = 100 * 100
+puts multiplication
+
+#割り算
+division = 100 / 100
+puts division
---
+++ HelloWorld.rb
@@ -0,0 +1,8 @@
+class Hello
+    def hello
+        puts 'Hello World'
+    end
+end
+
+greeting = Hello.new
+greeting.hello
